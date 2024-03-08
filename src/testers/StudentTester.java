cbec0bfd2ffd3875ade078685adb329adfe6f251
nataliavera6 <natalia.vera6@upr.edu>
2024-03-08 11:23:39 -0400

    added ballot,candidate and election classes

--- src/testers/StudentTester.java
+++ src/testers/StudentTester.java
@@ -1,6 +1,7 @@
 package testers;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
